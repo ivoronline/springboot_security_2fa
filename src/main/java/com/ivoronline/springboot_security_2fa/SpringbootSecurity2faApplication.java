036a2cd3e360e3a580a2b67a61aab40794b99d83
igoreta <ivoronline@gmail.com>
2021-01-30 10:31:59 +0100

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_security_2fa/SpringbootSecurity2faApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_security_2fa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurity2faApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootSecurity2faApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ivoronline/springboot_security_2fa/SpringbootSecurity2faApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_security_2fa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurity2faApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
